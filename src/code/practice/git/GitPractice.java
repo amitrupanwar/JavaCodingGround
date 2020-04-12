a4905610800aabdb80a7efa81ed113085fb02c79
Amit Rupanwar <52096459+amitrupanwar@users.noreply.github.com>
2020-04-12 21:42:52 +0530

    change from source tree checkout

--- src/code/practice/git/GitPractice.java
+++ src/code/practice/git/GitPractice.java
@@ -4,5 +4,7 @@ public class GitPractice {
 	// Line Added from source tree checkout
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!");
+		
+		System.out.println("Addition from source tree checkout");
 	}
 }
