7f5cdca1dc2cbcd0a704c9982f0d1a249e653127
Amit Rupanwar <52096459+amitrupanwar@users.noreply.github.com>
2020-04-12 21:18:46 +0530

    adding File for git practice

---
+++ src/code/practice/git/GitPractice.java
@@ -0,0 +1,8 @@
+package code.practice.git;
+
+public class GitPractice {
+	// Line Added from source tree checkout
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!!");
+	}
+}
